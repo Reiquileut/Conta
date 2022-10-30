f5529ba56bfaccca90b22b0c5bc9004193e226e5
Reiquileut <thiagosoteroprado@gmail.com>
2022-10-30 10:01:07 -0300

    Requisitos parciais

---
+++ Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private int idade;
+    private int contato;
+
+
+}
--- Conta.java
+++ Conta.java
@@ -1,5 +1,13 @@
 public class Conta {
     private double saldo;
+    private int agencia;
+    private int conta;
+    private Cliente titular;
+
+
+
+
+
 
 
 
