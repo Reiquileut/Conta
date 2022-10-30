09e48222c1585e63499ac96bdf37998b43e5aca8
Walessonrdreis <wallllase@hotmail.com>
2022-10-29 22:12:09 -0300

    first project commit

---
+++ NossoBanco/src/main/java/com/nossobanco/br/NossoBancoApplication.java
@@ -0,0 +1,13 @@
+package com.nossobanco.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoBancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NossoBancoApplication.class, args);
+    }
+
+}
---
+++ NossoBanco/src/test/java/com/nossobanco/br/NossoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nossobanco.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoBancoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
