32dcccc01571b3457034019c41020b2673fffdda
Reiquileut <78445783+Reiquileut@users.noreply.github.com>
2022-10-30 17:04:22 -0300

    Requisitos

--- Cliente.java
+++ Cliente.java
@@ -3,6 +3,13 @@ public class Cliente {
     private String cpf;
     private int idade;
     private int contato;
+    private String profissao;
+    private double patrimonio;
+    private boolean conjuge;
+
+
+
+
 
 
 }
