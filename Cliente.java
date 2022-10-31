2ef3d139a51cec434001d0dd0604c383bcddb239
Reiquileut <thiagosoteroprado@gmail.com>
2022-10-30 21:57:53 -0300

    Get and set

--- Cliente.java
+++ Cliente.java
@@ -10,6 +10,59 @@ public class Cliente {
 
 
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getContato() {
+        return contato;
+    }
+
+    public void setContato(int contato) {
+        this.contato = contato;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public double getPatrimonio() {
+        return patrimonio;
+    }
+
+    public void setPatrimonio(double patrimonio) {
+        this.patrimonio = patrimonio;
+    }
+
+    public boolean isConjuge() {
+        return conjuge;
+    }
+
+    public void setConjuge(boolean conjuge) {
+        this.conjuge = conjuge;
+    }
 }
--- Conta.java
+++ Conta.java
@@ -5,11 +5,27 @@ public class Conta {
     private Cliente titular;
 
 
+    public double getSaldo() {
+        return saldo;
+    }
 
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
+    public int getAgencia() {
+        return agencia;
+    }
 
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 
+    public int getConta() {
+        return conta;
+    }
 
-
-
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
 }
